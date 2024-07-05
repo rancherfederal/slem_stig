a7c5ef4a53d88eede6151f15bc386b9a46a37660
Majus457 <132930386+Majus457@users.noreply.github.com>
2024-07-05 15:37:41 -0600

    Update stig_high.sh

--- stig_high.sh
+++ stig_high.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Ensure the script is run as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root. Exiting."
+    exit 1
+fi
+
 LOGFILE="stig_high.log"
 
 # Make a new logfile

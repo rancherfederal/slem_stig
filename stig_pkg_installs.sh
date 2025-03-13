665655e0de5c1c688568feee0ac28e96bc1315d2
Majus457 <132930386+Majus457@users.noreply.github.com>
2025-03-12 21:51:33 -0600

    Create stig_pkg_installs.sh
    This file is used to install packages for meeting STIG requirements. 
    
    Since SLE Micro is transactional, the install of a package is typically done with transactional-update pkg install <package_name> and a reboot. When you have multiple packages to install, you can use transactional-update shell to enter an interactive shell where you can run multiple things and it counts as a single transaction.

---
+++ stig_pkg_installs.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Ensure the script is run as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root. Exiting."
+    exit 1
+fi
+
+LOGFILE="stig_pkg_installs.log"
+
+# Make a new logfile
+> "$LOGFILE"
+
+# Function to log messages
+log_message() {
+    local function_name="$1"
+    local message="$2"
+    echo "$function_name: | $message" >> "$LOGFILE"
+}
+
+# Packages to be installed per STIG's from the following Vulnerability IDs
+# KBD                   V-261276
+# MFA                   V-261396
+# AIDE                  V-261403
+# AIDE AUDIT            V-261410
+# AUDIT AUDISPD PLUGINS V-261412
+
+install_pkgs() {
+    local function_name="install_pkgs"
+    local pkgs=("pam_pkcs11" "mozilla-nss" "mozilla-nss-tools" "pcsc-ccid" "pcsc-lite" "pcsc-tools" "opensc" "coolkey" "kbd" "aide" "audit" "audit-audispd-plugins")
+
+    # Using a transactional shell to install packages in a single session
+    transactional-update shell <<EOF
+    zypper --non-interactive install ${pkgs[@]}
+EOF
+
+    # Check if transactional shell executed successfully
+    if [[ $? -eq 0 ]]; then
+        log_message "$function_name" "SLEM 5 packages have been installed successfully in a transactional session."
+    else
+        log_message "$function_name" "Failed to install SLEM 5 packages within the transactional session."
+        exit 1
+    fi
+}
+
+install_pkgs
